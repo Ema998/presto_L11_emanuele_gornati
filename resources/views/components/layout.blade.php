0ecd2c40da078de26dfa041cd4043172dba740af
Ema998 <emanuele.gornati3@gmail.com>
2025-05-20 07:03:29 +0200

    correzione rotte navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,11 +18,11 @@
                     <a class="nav-link" href="{{ route('homepage') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('articles.index') }}">I Nostri Articoli</a>
+                    <a class="nav-link" href="{{ route('article.index') }}">I Nostri Articoli</a>
                 </li>
                 @auth
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('articles.create') }}">Crea articolo</a>
+                        <a class="nav-link" href="{{ route('article.create') }}">Crea articolo</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Ciao {{ Auth::user()->name }}</a>
