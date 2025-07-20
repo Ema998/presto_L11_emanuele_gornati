9a560da7bae5d742585efe4516ceeb9ac30f3d53
Ema998 <emanuele.gornati3@gmail.com>
2025-07-20 18:24:28 +0200

    commit

--- lang/it/ui.php
+++ lang/it/ui.php
@@ -2,5 +2,25 @@
 
 return
     [
+        'middlewareIsRevisorError' => 'Accesso negato',
 
+        'createArticleFormMessage' => 'Articolo creato con successo',
+
+        'byCategoryTitle' => 'Articoli della categoria',
+        'byCategoryMessage' => 'Nessun articolo trovato per la categoria selezionata',
+
+        'createTitle' => 'Crea un articolo',
+
+        'indexTitle' => 'I nostri articoli',
+
+        'searchedTitle' => 'Risultati della ricerca',
+        'searchedMessage' => 'Nessun articolo trovato',
+
+        'showPrevious' => 'Precedente',
+        'showNext' => 'Successivo',
+        'showMessage' => 'Non sono presenti foto',
+        'showPrice' => 'Prezzo',
+        'showCategory' => 'Categoria',
+
+        'loginTitle' => 'Login',
     ];
