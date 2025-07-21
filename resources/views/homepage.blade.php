3b1532f0b00ecc2e134cc1355117b19a6ee4f1d9
Ema998 <emanuele.gornati3@gmail.com>
2025-07-21 22:45:43 +0200

    commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,10 +3,10 @@
         <h1 class="text-center">Presto.it</h1>
     </x-header>
     <x-errors />
-    <div class="container">
-        <div class="row justify-content-center align-items-center">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
             @forelse ($articles as $article)
-                <div class="col-12 col-md-3">
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                     <x-card :article="$article" />
                 </div>
             @empty
